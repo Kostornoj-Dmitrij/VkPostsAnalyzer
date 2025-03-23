540b59ae0a105561b5471b0ba06e03b4011468c5
Kostornoj-Dmitrij <D1.imosa@yandex.ru>
2025-03-23 03:28:00 +0300

    Program logging update

--- Program.cs
+++ Program.cs
@@ -5,23 +5,36 @@ using VkPostsAnalyzer.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseSerilog((_, config) =>
+{
+    config.WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+          .WriteTo.Console();
+});
+
+Log.Information("Starting application configuration...");
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddHttpClient<VkService>();
-
 builder.Services.AddScoped<VkService>();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Host.UseSerilog((context, config) =>
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+if (string.IsNullOrEmpty(connectionString))
 {
-    config.WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day);
-});
+    Log.Error("Connection string 'DefaultConnection' is missing or empty.");
+    throw new InvalidOperationException("Connection string 'DefaultConnection' is missing or empty.");
+}
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+Log.Information("Application configuration completed.");
 
 var app = builder.Build();
+
+Log.Information("Applying database migrations...");
 
 using (var scope = app.Services.CreateScope())
 {
@@ -32,11 +45,21 @@ using (var scope = app.Services.CreateScope())
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "VK Posts Analyzer API V1");
+        c.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
+var swaggerUrl = "http://localhost:5144/swagger";
+Log.Information("Application is starting...");
+Log.Information($"Swagger UI is available at: {swaggerUrl}");
+
 app.Run();
+
+Log.Information("Application has stopped.");
